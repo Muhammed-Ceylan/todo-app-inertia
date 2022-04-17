c6526ae88f13208d8780bf27ac31211567d423dd
Muhammed-Ceylan <mmuhammedceylan@gmail.com>
2022-04-17 16:21:10 +0300

    Install Laravel with Jetstrean and Inertia. Then setup base structre.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Article;
+use App\Models\User;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::factory(10)->create();
+
+        $users->each(function ($user) {
+           $user->articles()->saveMany(Article::factory(rand(3,10))->make());
+        });
+    }
+}
---
+++ resources/js/Pages/Articles/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Burası Index.vue sayfasıdır.
+    </div>
+    <hr/>
+    <ul>
+        <li v-for ="article in articles" :key="article.id">
+            {{ article.title }}
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: ['articles'],
+    }
+</script>
